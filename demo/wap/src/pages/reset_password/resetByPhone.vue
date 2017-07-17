9e24d88b864a5935a19c9d72f8b648186a088b20
c.c <469770982@qq.com>
2017-07-17 16:21:30 +0800

    version

---
+++ demo/wap/src/pages/reset_password/resetByPhone.vue
@@ -0,0 +1,114 @@
+
+<template>
+    <div>
+           <header class="border-1px">
+            <span class="c-head-cancel f-vermiddle" @click="cancel"> 
+                     Cancel
+                </span>
+            <span class="c-head-title f-cenmiddle">
+                    Password Reset
+                </span>
+            <span class="c-head-next" @click="next" :style="{'color':nextActive}">  
+                      Submit
+                </span>
+        </header>
+        <section class="reset-password border-1px">
+            <input type="password" class="f-boxsizing" v-model.trim="newPassword" placeholder="New Password (6-25 characters)">
+            <input type="password" class="f-boxsizing" v-model.trim="confirmPassword" placeholder="Confirm Password">
+        </section>
+    </div>
+</template>
+
+<script>
+import { MessageBox } from "mint-ui";
+export default {
+    data() {
+        return {
+         accountData:this.$route.params.accountData,
+           newPassword: null,
+           confirmPassword: null
+        };
+    },
+  computed: {
+    //  改变next颜色
+   nextActive() {
+    // alert(this.newPassword);
+       return this.newPassword && this.confirmPassword ? "#ff0030": "";
+   }
+    },
+    methods: {
+    //  取消    
+    cancel() {
+           
+            MessageBox({
+            title: " ",
+            message: "Are you sure want to leave this page?",
+            showCancelButton: true,
+            confirmButtonText:"Confirm",
+            cancelButtonText:"Cancel",
+            confirmButtonClass: "messagebox-confirm-btn",
+            cancelButtonClass: "messagebox-cencel-btn"
+        })
+        .then(action => {
+                action==="confirm" ? this.$router.replace("/login") : "";
+            });
+        },
+    //  下一步
+    next() {
+      let a = () => {
+      let ressetPassword = /(?!^\\d+$)(?!^[a-zA-Z]+$)(?!^[_#@]+$).{6,}/;
+      let newPass = this.newPassword;
+      let confirmPass = this.confirmPassword;
+     
+      if (ressetPassword.test(newPass) && ressetPassword.test(confirmPass)) {
+      newPass===confirmPass ? this.sendUpdate() : alert("Please input the match password.");             
+      } else {
+          alert("Password must be 6-25 characters and containing number and letter.");
+      }
+    };
+    if (this.newPassword && this.confirmPassword) {
+        a();
+    }
+
+},
+   //  提交重置的密码
+    sendUpdate() {
+          let params = {
+          selectType: 1,
+          sign: this.accountData.sign,
+          resetCode: this.accountData.resetCode,
+          userId: this.accountData.userId,
+          newPassword:this.newPassword,
+          newPasswordConfirm: this.confirmPassword
+        };
+         this.axios({   
+          method: "post",
+          url: "/user/moblieWayToPasswordUpdate.cf",
+          params: params
+        })
+        .then((res) => {
+            if (res.data.status==="success") {
+             this.$router.replace({name:"resetSuccess", params:{type:2}});
+            }
+        });
+    }
+    }
+};
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins/index.styl"
+.reset-password
+ padding-top: 2.8rem
+ position: relative
+ input:first-child
+  border-bottom .5px solid rgba(0, 0 ,0 ,.12)
+ input
+  padding-left: .6rem
+  height: $cell-height
+  width: 100%
+  font-size: .8rem
+  color: rgba(0,0,0,0.86)
+  letter-spacing: -0.81px 
+</style>
---
+++ demo/wap/src/pages/reset_password/verifyCode.vue
@@ -0,0 +1,132 @@
+<template>
+    <div>
+     <header class="border-1px">
+            <span class="c-head-cancel f-vermiddle" @click="cancel"> 
+                     Cancel
+                </span>
+            <span class="c-head-title f-cenmiddle">
+                    Password Reset
+                </span>
+            <span class="c-head-next" @click="next" :style="{'color':nextActive}">  
+                      Next
+                </span>
+        </header>
+      <section class="passmob-tip">
+        <p>A password reset code has been sent to</p>
+        <p>{{accountData.mobile}}, please check!</p> 
+      </section>
+      <section class="passmob-code">
+        <input type="text" pattern="\d*" v-model.trim="code" placehold="Password reset code">
+        <div class="passmob-send-btn f-vermiddle" @click="checkSented"><span v-if="!showTime">{{time+ " S"}}</span> <span v-if="showTime">Sent Again</span></div>
+      </section>
+      
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accountData: this.$route.params.accountData,  //   保存账户信息
+      code: null,   //  验证码
+      time: 60,   //  限制时间
+      showTime:false  //  是否显示时间
+    };
+  },
+  created() {
+    this.intervalTime();
+  },
+  computed: {
+    //  改变next颜色
+    nextActive() {
+     return this.code && "#ff0030";
+    }
+  },
+  methods:{
+    //  时间倒计时
+    intervalTime() {
+      this.showTime = false;
+      this.time = 60;
+   let interval = setInterval(() => {
+        this.time--;
+        if (this.time===0) {
+          clearInterval(interval);
+          this.showTime = true;
+        }
+      }, 1000);
+    },
+    //  重新拉取验证码
+    checkSented() {
+      this.showTime && this.sentAgain();
+    },
+    sentAgain() {
+         let params = {
+          selectType: 1,
+          sign: this.accountData.sign,
+          userId: this.accountData.userId
+        };
+        this.axios({
+          method: "post",
+          url: " /user/sendMobileCode.cf",
+          params: params
+        })
+        .then((res) => {
+          res.data.status==="success"? this.intervalTime() : alert(res.data.message);
+        });
+    },
+
+
+    cancel() {
+      this.$router.go(-1);
+    },
+    next() {
+      this.code && this.sendMobCode();
+    },
+    //  发送验证码
+    sendMobCode () {
+         let params = {
+          selectType: 1,
+          sign: this.accountData.sign,
+          resetCode: this.code,
+          userId: this.accountData.userId
+        };
+         this.axios({
+          method: "post",
+          url: "/user/validateResetCode.cf",
+          params: params
+        })
+        .then((res) => {
+         res.data.status==="success" ? this.$router.replace({name:"resetByPhone", params:{accountData:res.data.data}}) : alert(res.data.message);
+        });
+    }
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins/index.styl"
+.passmob
+ &-tip
+  padding: 2.8rem .6rem .6rem
+  font-size: .8rem
+  color: rgba(0,0,0,0.26)
+  letter-spacing: -0.61px
+  line-height: 1rem
+ &-code
+  position relative
+  background: #fff
+  input
+   font-size: .8rem
+   height: $cell-height
+   padding-left:.6rem
+   color: rgba(0,0,0,0.86)
+ &-send-btn
+   padding: .3rem .4rem
+   font-size: .8rem
+   color: #fff
+   text-align center
+   border-radius: 4px
+   -webkit-border-radius: 4px
+   background: #ff0030
+   right: $right-24
+</style>
